5f0d660bb95c397c47ba3d49d4ba391d8e996a6e
jhonatanrafael2010-ship-it <jhonatanrafael2010@gmail.com>
2025-10-28 23:00:53 -0400

    Att

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import "./styles/App.css";
 import { Moon, SunMedium } from "lucide-react";
 import { syncPendingVisits } from "./utils/offlineSync";
 import MobileMenu from "./components/MobileMenu";
-import "./MobileMenu.css";
+
 
 
 const App: React.FC = () => {
